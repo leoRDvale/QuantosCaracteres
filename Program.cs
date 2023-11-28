47a88e9694a6d858c8040df9b70c36ea41976cc3
LeoVale <151870779+leoRDvale@users.noreply.github.com>
2023-11-28 17:26:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//digite um caractere para ser encontrado no texto.
+
+string frase = "nao e a linguagem de programacao que define o programador, mas sim sua logica.";
+
+void caracter(string frase) {
+
+    int contador = 0;
+    string msg = "";
+
+    Console.WriteLine($"Digite um caracter para ser encontrado na frase: {frase}");
+    char digitado = Convert.ToChar(Console.ReadLine());
+
+    for (int i = 0; i < frase.Length; i++) {
+        if (digitado == frase[i]) { contador += 1;}
+
+    }
+    msg = contador != 0 ? $"Total de caracteres encontrados: {contador} \nCaracter: {digitado}" : $"Caracter nao encontrado \nCaracter: {digitado
+    }";
+    Console.WriteLine(msg);
+
+}
+caracter(frase);
+
